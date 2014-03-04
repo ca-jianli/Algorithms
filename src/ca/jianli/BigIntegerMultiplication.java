4be0a2c541f94743561b66c47249196687d597df
Jian Li <me@jianli.ca>
2014-03-03 18:00:47 -0800

    Added some more comments

--- src/ca/jianli/BigIntegerMultiplication.java
+++ src/ca/jianli/BigIntegerMultiplication.java
@@ -2,7 +2,13 @@ package ca.jianli;
 
 /**
  * Implement multiplication of large integers, overflow possible therefore
- * representing using array
+ * representing using array.
+ * 
+ * This can be extended to a stream of integer input b, multiplying by a known
+ * integer a.
+ * 
+ * This version is more cache-friendly compared with the standard long
+ * multiplication.
  * 
  * @author Jian Li
  */
@@ -16,6 +22,11 @@ public class BigIntegerMultiplication {
 
 		for (int j = b_size - 1; j >= 0; j--) {
 			int carry = 0;
+
+			// buffer tracks the intermediate sum, buffer[a_size] is calculated
+			// for each iteration of j, and then never changed again. So assign
+			// it to result at the end of each loop.
+
 			for (int i = a_size; i > 0; i--) {
 				buffer[i] = buffer[i - 1] + b[j] * a[i - 1] + carry;
 				carry = buffer[i] / 10;
@@ -24,6 +35,8 @@ public class BigIntegerMultiplication {
 			buffer[0] = carry;
 			res[a_size + j] = buffer[a_size];
 		}
+
+		// flush the rest of sum digits from buffer to result
 
 		for (int i = a_size - 1; i >= 0; i--) {
 			res[i] = buffer[i];
