145f9c5c171d2a4c02389e48d4956b2db2725dce
Jian Li <me@jianli.ca>
2014-03-11 17:22:04 -0700

    Added some comment.

--- src/ca/jianli/LongestIncreasingSubsequence.java
+++ src/ca/jianli/LongestIncreasingSubsequence.java
@@ -6,22 +6,23 @@ import java.util.ArrayList;
 /**
  * Find the longest increasing subsequence in an array of integers. O(nlogn)
  * http://en.wikipedia.org/wiki/Longest_increasing_subsequence
+ * 
  * @author Jian Li
  * 
  */
 public class LongestIncreasingSubsequence {
 
 	public static ArrayDeque<Integer> lsi(int[] input) {
-		// longest(i) is the index k of input[k], which is the last element of 
+		// longest(i) is the index k of input[k], which is the last element of
 		// a increasing subsequence of length i.
 		// longest(0) is not valid for non-trivial input, and has a value of -1.
-		
+
 		ArrayList<Integer> longest = new ArrayList<Integer>();
 		longest.add(-1);
-		
+
 		// prev[i] contains the previous element index of the last element of a
-		// increasing subsequence. 
-		
+		// increasing subsequence.
+
 		int[] prev = new int[input.length];
 		for (int i = 0; i < input.length; i++) {
 			int insertLocation = bst(input, longest, i);
@@ -34,7 +35,7 @@ public class LongestIncreasingSubsequence {
 		}
 
 		// build longest subsequence output from prev[]
-		
+
 		ArrayDeque<Integer> output = new ArrayDeque<Integer>();
 		int last = longest.get(longest.size() - 1);
 		while (last != -1) {
@@ -44,6 +45,15 @@ public class LongestIncreasingSubsequence {
 		return output;
 	}
 
+	/**
+	 * Find the location in existing longest subsequence to insert element
+	 * input[i] so that it can be used to form increasing subsequence.
+	 * 
+	 * @param input
+	 * @param longest
+	 * @param i
+	 * @return
+	 */
 	private static int bst(int[] input, ArrayList<Integer> longest, int i) {
 		int lo = 1;
 		int hi = longest.size() - 1;
