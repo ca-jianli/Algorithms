c8fe905c876fbddd88ef6184d690839e315becf8
Jian Li <me@jianli.ca>
2014-03-14 23:55:42 -0700

    Added peaks question.

---
+++ src/ca/jianli/PeaksTest.java
@@ -0,0 +1,18 @@
+package ca.jianli;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PeaksTest {
+
+	@Test
+	public void test1() {
+		assertEquals(3, Peaks.solution(new int[] { 0, 1, 0, 1, 0, 1, 0, 1, 0 }));
+		assertEquals(1, Peaks.solution(new int[] { 0, 1, 0 }));
+		assertEquals(0, Peaks.solution(new int[] { 0 }));
+		assertEquals(0, Peaks.solution(new int[] { 0, 1, 2, }));
+		assertEquals(3, Peaks.solution(new int[] { 1, 2, 3, 4, 3, 4, 1, 2, 3,
+				4, 6, 2 }));
+	}
+}
