c4704050508919f7485a8dd9d752c65468f2c0d2
Jian Li <me@jianli.ca>
2014-03-15 10:55:25 -0700

    Added codility peaks and flags question.

---
+++ src/ca/jianli/PlantingFlagsTest.java
@@ -0,0 +1,18 @@
+package ca.jianli;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlantingFlagsTest {
+
+	@Test
+	public void test1() {
+
+		assertEquals(
+				3,
+				PlantingFlags.solution(new int[] { 1, 5, 3, 4, 3, 4, 1, 2, 3,
+						4, 6, 2 }));
+	}
+
+}
