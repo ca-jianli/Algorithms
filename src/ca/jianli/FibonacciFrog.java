49332eb6aa39218d9594ab02a6d6e95003387d1d
Jian Li <me@jianli.ca>
2014-03-16 00:08:07 -0700

    Optimized Fib Frog time complexity to O(N log N)

--- src/ca/jianli/FibonacciFrog.java
+++ src/ca/jianli/FibonacciFrog.java
@@ -1,48 +1,68 @@
 package ca.jianli;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Queue;
 
+/**
+ * Count the minimum number of jumps required for a frog to get to the other
+ * side of a river.
+ * 
+ * Input array consists of 1's and 0's, one represent leaf that frog can land
+ * on, 0 is water.
+ * 
+ * Frog can make Fibonacci numbered jumps only.
+ * 
+ * @author Jian Li
+ * 
+ */
 public class FibonacciFrog {
 
 	public static int solution(int[] A) {
-		// write your code in Java SE 7
-		ArrayList<Integer> river = new ArrayList<Integer>();
-		for (int a : A) {
-			river.add(a);
-		}
-		river.add(1);
-
+		// find all fib numbers for size of A
 		ArrayList<Integer> fib = new ArrayList<Integer>();
 		int a = 0;
 		int b = 1;
 		while (true) {
 			int x = a + b;
-			if (x > river.size())
+			if (x > A.length + 1)
 				break;
 			fib.add(x);
 			a = b;
 			b = x;
 		}
 
-		HashMap<Integer, Integer> prevs = new HashMap<Integer, Integer>();
-		prevs.put(-1, 0);
-		for (int i = 0; i < river.size(); i++) {
-			if (river.get(i) == 1) {
-				int min = Integer.MAX_VALUE;
+		// maintain a queue of starting locations for jump, and do a BFS using
+		// all fib numbers; number of fib numbers is O(logN)
+
+		Queue<Integer> next = new ArrayDeque<Integer>();
+		next.add(-1); // add starting location on one side of river
+
+		int numJumps = 1;
+		while (!next.isEmpty()) {
+			int size = next.size();
+			for (int i = 0; i < size; i++) {
+				int from = next.remove();
 				for (int f : fib) {
-					if (prevs.containsKey((i - f))) {
-						min = Math.min(min, prevs.get(i - f) + 1);
+					int to = from + f;
+					if (to == A.length) {
+						// reached destination
+						return numJumps;
+					} else if (to > A.length || A[to] == 0) {
+						// no leaf to land on, or exceeded target, or already
+						// visited
+					} else {
+						next.add(to);
+						// mark as visited
+						A[to] = 0;
 					}
 				}
-				if (min != Integer.MAX_VALUE) {
-					prevs.put(i, min);
-				}
 			}
+			numJumps++;
 		}
+		// no more place to jump from and haven't reached other end.
 
-		return prevs.containsKey(river.size() - 1) ? prevs
-				.get(river.size() - 1) : -1;
+		return -1;
 	}
 
 	public static void main(String[] arg) {
